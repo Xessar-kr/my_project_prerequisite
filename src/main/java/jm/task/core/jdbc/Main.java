c6ff0ad629ada076be70efb64276ef0245c2d88c
Xessar <xessar.sakh@gmail.com>
2024-12-11 10:29:44 +0900

    Realized Main, User, JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // implement algorithm here
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+        userDao.saveUser("Ivan", "Ivanov", (byte) 20);
+        userDao.saveUser("Svetlana", "Belova", (byte) 30);
+        userDao.saveUser("Sam", "Samov", (byte) 40);
+        userDao.saveUser("Elena", "Troyanskaya", (byte) 50);
+        List<User> users = userDao.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+//        userDao.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/Connection1";
+    private static final String URL = "jdbc:mysql://localhost:3306/test_schema";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
 
